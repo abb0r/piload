ba329e0524f921442e20e78efa41ac2de93f75bc
abb0r <abbi1988@gmail.com>
2026-09-03 23:57:22 +0000

    v0.1.6: version string

--- desktop/piload.py
+++ desktop/piload.py
@@ -22,7 +22,7 @@ import paramiko
 
 APP_DIR = Path(os.environ.get("APPDATA") or Path.home() / ".piload") / "PiLoad"
 SETTINGS = APP_DIR / "settings.json"
-VERSION = "0.1.5"
+VERSION = "0.1.6"
 REPO_URL = "https://github.com/abb0r/piload"
 
 PRESETS = {
